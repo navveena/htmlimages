109ba989aa0f6854f0b4adee03948487e2e13499
Naveena <navvna22@gmail.com>
2023-10-04 20:00:33 +0530

    added files

---
+++ htmlimagesapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HtmlimagesappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'htmlimagesapp'
---
+++ htmlimagesapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+
+def naveena(request):
+    return render(request,"index.html")
